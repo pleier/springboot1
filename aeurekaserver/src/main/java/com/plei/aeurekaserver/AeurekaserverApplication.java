15bccd4739621748a6a8f2efb8b1add2c0ddd891
plei <pleiyang@outlook.com>
2018-03-15 10:50:23 +0800

    init

---
+++ aeurekaserver/src/main/java/com/plei/aeurekaserver/AeurekaserverApplication.java
@@ -0,0 +1,12 @@
+package com.plei.aeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeurekaserverApplication.class, args);
+	}
+}
